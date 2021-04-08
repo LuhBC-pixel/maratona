e89f011dae22434a5d76c89fc5f97a157db42e31
LuisaBC-pixel <bc.luisa@yahoo.com.br>
2021-04-07 21:40:46 -0300

    Aula 4 (o verdadeiro)

--- src/model/Job.js
+++ src/model/Job.js
@@ -1,28 +1,45 @@
-let data = [
-  {
-    id: 1,
-    name: 'Pizzaria Guloso',
-    'daily-hours': 2,
-    'total-hours': 1,
-    created_at: Date.now()
-  },
-  {
-    id: 2,
-    name: 'OneTwo Project',
-    'daily-hours': 3,
-    'total-hours': 47,
-    created_at: Date.now()
-  },
-];
+const Database = require('../db/config');
 
 module.exports = {
-  get() {
-    return data;
+  async get() {
+    const db = await Database();
+
+    const data = await db.all(`SELECT * FROM jobs`);
+
+    await db.close();
+
+    return data.map(job => ({
+      id: job.id,
+      name: job.name,
+      'daily-hours': job.daily_hours,
+      'total-hours': job.total_hours,
+      created_at: job.created_at
+    }));
   },
+
   update(newJob) {
     data = newJob;
   },
+
   delete(id) {
     data = data.filter(job => Number(job.id) !== Number(id));
+  },
+
+  async create(newJob) {
+    const db = await Database();
+
+    await db.run(`INSERT INTO jobs (
+      name,
+      daily_hours,
+      total_hours,
+      created_at
+    ) VALUES (
+      '${newJob.name}',
+      ${newJob['daily-hours']},
+      ${newJob['total_hours']},
+      ${newJob.created_at}
+    )`);
+
+    await db.close();
   }
 }
